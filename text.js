3a2605829ac64ed7f7f02dcbebfcf192dad31a99
yuzhu chai <yzhuchai@gmail.com>
2022-01-22 12:43:55 +0800

    navigation and logic solid for first 5 paragraphs

--- text.js
+++ text.js
@@ -14,7 +14,6 @@ function updateTimeZone(){
     let date = new Date();
     if(zone == "local"){
         timeString =  date.toLocaleString("en-US", {hour12: false})
-
     } else {
         timeString =  date.toLocaleString("en-US", {timeZone: zone, hour12: false});
     }
@@ -22,18 +21,14 @@ function updateTimeZone(){
     d = new Date(timeString)
     d.setSeconds(d.getSeconds() - 10*enableDelay)
     d.setHours(d.getHours() - delayAnHour)
-    console.log(d)
-
+    // console.log(d)
     timeDisplay.text(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds())
     setTimeout(updateTimeZone, 1000);
 }
-//this is to test the time zone change when triggered with zone variable
-// $('#testTimeChange').on('click',()=>{
-//     zone = "America/New_York"
-// })
 
 
-// display texts functions --------------------------------------------------
+
+// display texts functions ----------------------------------------------------------
 
 let p02 = $('#p02').html()
 let p02display = false 
@@ -44,42 +39,96 @@ let p03display = false
 let p04 = $('#p04').html()
 let p04display = false
 
+let p05 = $('#p05').html()
+let p05display = false
+
+console.log()
 
 //P01 "delayed" 
-$('#delay01').on('click',()=>{
+$('#text').on('click','#delay01',()=>{
     $("#delay01").addClass('clickedLink')
     $('#delayAnHour').addClass('clickedLink')
     if(!p02display){
         $("#text").append("<div id='p02show'>"+ p02 +"</div>")
     }
     p02display = true;
+    // $('.read04').contents().unwrap();
+    // $("br",'#p04').remove();
+    // $("#p04").html(p04+"<span class='next04'>[next]</span>")
 })
 
-//P02 "an hour" 
+//P01 "an hour" 
 $('#delayAnHour').on("click", ()=>{
     delayAnHour = 1
     if(!p02display){
         $('#text').append("<div id='p02show'>" + p02 + "</div>")
     }
     p02display = true;
+    // $("br",'#p04').remove();
+    // $("#p04").html(p04+"<span class='next04'>[next]</span>")
     $("#delayAnHour").addClass('clickedLink');
     $("#delay01").addClass('clickedLink')
 })
 
 
-//P04 "the city"
+//P01 "the city"
 $("#theCity01").on('click',()=>{
     if(!p04display){
+        
         $('#text').append("<div id='p04show'>" + p04 + "</div>")
     }
     p04display = true;
     $('#theCity01').addClass('clickedLink')
 })
 
+ 
+//P02 "write", need to bound the onclick function to the parent div, JQuery OnClick Method is bound to an element or selector on page ready/load. 
+$('#text').on('click', '.write02', ()=>{
+    console.log(p03display)
+    if(!p03display){
+        console.log('p03not shown')
+        $('#text').append("<div id='p03show'>" + p03 + "</div>")
+    }
+    p03display = true;
+    $(".write02").addClass('clickedLink')
+})
 
+
+$('#text').on('click', '.next03',()=>{
+    if(!p04display){
+        $('.read04').contents().unwrap();
+        $("br",'#p04').remove();
+        let newp04 = $('#p04').html()
+        $('#text').append("<div id='p04show'>" + newp04 + "<span class='next04'>[next]</span><br/><br></div>")
+        p04display = true;
+        $('#theCity01').addClass('clickedLink')
+    } else {
+        $('#text').append("<div id='p05show'>" + p05 + "</div>")
+        p05display = true;
+    }
+    $('.next03').remove()
+})
+
+$('#text').on('click', '.next04',()=>{ 
+    $('#text').append("<div id='p05show'>" + p05 + "</div>")
+    p05display = true; 
+    $('.next04').remove()
+})
+
+
+
+$('#text').on('click', '.read04',()=>{
+    if(!p02display){
+        $('#text').append("<div id='p02show'>" + p02 + "</div>")
+        p02display = true
+    } 
+    $('.read04').addClass('clickedLink')
+    $("#delayAnHour").addClass('clickedLink');
+    $("#delay01").addClass('clickedLink')
+})
 
 // change time zone functions -----------------------------------------
-//change time when NY is clicked 
+//change time when "NY" is clicked 
 $(".nyTime").on('click',()=>{
     zone = "America/New_York"
 })
