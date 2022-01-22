f5fcda32a1c99e91726070d547f33207cd7b1b8c
yuzhu chai <yzhuchai@gmail.com>
2022-01-22 10:22:18 +0800

    able to delay an hour

--- text.js
+++ text.js
@@ -5,6 +5,7 @@ let hour
 let minute 
 let second
 let enableDelay = 0
+let delayAnHour = 0
 
 function updateTimeZone(){
     // "America/New_York"
@@ -17,9 +18,10 @@ function updateTimeZone(){
     } else {
         timeString =  date.toLocaleString("en-US", {timeZone: zone, hour12: false});
     }
-    
+    //delay time every 10 seconds when delay is clicked
     d = new Date(timeString)
     d.setSeconds(d.getSeconds() - 10*enableDelay)
+    d.setHours(d.getHours() - delayAnHour)
     console.log(d)
 
     timeDisplay.text(d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds())
@@ -31,22 +33,42 @@ function updateTimeZone(){
 // })
 
 
+let p02 = $('#p02').html()
+let p02display = false 
+
+let p03 = $('#p03').html()
+let p03display = false 
 
 let p04 = $('#p04').html()
 let p04display = false
+
 
 //when delay function clicks 
 $(".delay").on('click',()=>{
     enableDelay ++
     console.log("clicked", enableDelay)
+    if(!p02display){
+        $("#text").append("<div id='p02show'>"+ p02 +"</div>")
+    }
+    p02display = true;
 })
+
+
+$('#delayAnHour').on("click", ()=>{
+    delayAnHour = 1
+    if(!p02display){
+        $('#text').append("<div id='p02show'>" + p02 + "</div>")
+    }
+    p02display = true;
+})
+
 
 $(".nyTime").on('click',()=>{
     zone = "America/New_York"
 })
 $("#display04").on('click',()=>{
     if(!p04display){
-        $('#text').append(p04)
+        $('#text').append("<div id='p04show'>" + p04 + "</div>")
     }
     p04display = true;
 })
